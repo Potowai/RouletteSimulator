44cad1cab8927ac325da7836d380f6887fac3680
Alexis <alexis.thomas.ines@gmail.com>
2024-05-25 21:28:41 +0200

    edit table

--- app/components/RouletteTable.jsx
+++ app/components/RouletteTable.jsx
@@ -16,7 +16,6 @@ const RouletteTable = ({ addMethod }) => {
   };
 
   const redNumbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
-  const blackNumbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35];
 
   const getColorClass = (number) => {
     if (number === 0) return 'bg-green-600';
@@ -31,51 +30,37 @@ const RouletteTable = ({ addMethod }) => {
 
   return (
     <div className="flex justify-center mt-8">
-      <div className="bg-gray-800 p-4 rounded-lg shadow-lg ">
-        <div className="flex">
-          <div className="flex flex-col items-center justify-center bg-green-600 w-24 h-full rounded-l-lg">
-            <div className="text-white text-2xl font-bold p-8 cursor-pointer" onClick={() => handleCellClick(0)}>0</div>
+      <div className="bg-gray-800 p-4 rounded-lg">
+        <div className="flex bg-gray-700 rounded-r-lg">
+          <div className="row-span-3 flex items-center justify-center bg-green-600 rounded cursor-pointer h-full" onClick={() => handleCellClick(0)}>
+            <div className="text-white text-2xl font-bold p-8">0</div>
           </div>
-          <div className="grid grid-cols-12 gap-4 p-4 bg-gray-700 rounded-r-lg">
-            {numberRows[0].map((number) => (
-              <div key={number} className={`${getColorClass(number)} text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer h-10 w-10`} onClick={() => handleCellClick(number)}>{number}</div>
-            ))}
-            {numberRows[1].map((number) => (
-              <div key={number} className={`${getColorClass(number)} text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer h-10 w-10`} onClick={() => handleCellClick(number)}>{number}</div>
-            ))}
-            {numberRows[2].map((number) => (
-              <div key={number} className={`${getColorClass(number)} text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer h-10 w-10`} onClick={() => handleCellClick(number)}>{number}</div>
-            ))}
-            <div className="col-span-4 flex items-center justify-center">
-              <div className="bg-gray-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleDozenClick('1st12')}>1st12</div>
-            </div>
-            <div className="col-span-4 flex items-center justify-center">
-              <div className="bg-gray-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleDozenClick('2nd12')}>2nd12</div>
-            </div>
-            <div className="col-span-4 flex items-center justify-center">
-              <div className="bg-gray-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleDozenClick('3rd12')}>3rd12</div>
-            </div>
-            <div className="col-span-2 flex items-center justify-center">
-              <div className="bg-gray-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleSectionClick('1to18')}>1to18</div>
-            </div>
-            <div className="col-span-2 flex items-center justify-center">
-              <div className="bg-gray-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleSectionClick('EVEN')}>EVEN</div>
-            </div>
-            <div className="col-span-2 flex items-center justify-center">
-              <div className="bg-red-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleSectionClick('red')}>RED</div>
-            </div>
-            <div className="col-span-2 flex items-center justify-center">
-              <div className="bg-black text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleSectionClick('black')}>BLACK</div>
-            </div>
-            <div className="col-span-2 flex items-center justify-center">
-              <div className="bg-gray-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleSectionClick('ODD')}>ODD</div>
-            </div>
-            <div className="col-span-2 flex items-center justify-center">
-              <div className="bg-gray-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleSectionClick('19to36')}>19to36</div>
-            </div>
-          </div>
+        <div className="grid grid-cols-12 gap-2 p-4 ">
+          {numberRows[0].map((number) => (
+            <div key={number} className={`${getColorClass(number)} text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer h-24`} onClick={() => handleCellClick(number)}>{number}</div>
+          ))}
+          {numberRows[1].map((number) => (
+            <div key={number} className={`${getColorClass(number)} text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer h-24`} onClick={() => handleCellClick(number)}>{number}</div>
+          ))}
+          {numberRows[2].map((number) => (
+            <div key={number} className={`${getColorClass(number)} text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer h-24`} onClick={() => handleCellClick(number)}>{number}</div>
+          ))}
         </div>
       </div>
+        <div className="grid grid-cols-3 gap-4 mt-4">
+            <div className="bg-gray-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleDozenClick('1st12')}>1st12</div>
+            <div className="bg-gray-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleDozenClick('2nd12')}>2nd12</div>
+            <div className="bg-gray-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleDozenClick('3rd12')}>3rd12</div>
+          </div>
+          <div className="grid grid-cols-6 gap-4 mt-4">
+            <div className="bg-gray-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleSectionClick('1to18')}>1to18</div>
+            <div className="bg-gray-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleSectionClick('EVEN')}>EVEN</div>
+            <div className="bg-red-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleSectionClick('red')}>RED</div>
+            <div className="bg-black text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleSectionClick('black')}>BLACK</div>
+            <div className="bg-gray-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleSectionClick('ODD')}>ODD</div>
+            <div className="bg-gray-600 text-white text-2xl font-bold flex items-center justify-center rounded cursor-pointer w-full py-8" onClick={() => handleSectionClick('19to36')}>19to36</div>
+          </div>
+        </div>
     </div>
   );
 };
